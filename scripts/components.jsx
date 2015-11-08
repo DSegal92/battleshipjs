843464238009d8c476011d9cc799607ad56f675e
Daniel Segal <dsegal92@gmail.com>
2015-11-07 22:07:42 -0500

    Fetch game state from the server

--- scripts/components.jsx
+++ scripts/components.jsx
@@ -1,11 +1,19 @@
+
 var Cell = React.createClass({
   handleHover: function(){
+    console.log(this.props.value)
     this.props.onMouseEnter(this.props.x, this.props.y);
   },
   render: function() {
     var x = this.props.x;
     var y = this.props.y;
-    return <td className="tableCell" onMouseEnter={this.handleHover}></td>;
+    var cellClass = 'tableCell'
+
+    if (this.props.value == 'ship') cellClass += ' ship'
+    else if (this.props.value == 'damaged_ship') cellClass += ' damaged_ship'
+    else if (this.props.value == 'bad_guess') cellClass += ' bad_guess'
+
+    return <div className={cellClass} onMouseEnter={this.handleHover}></div>;
   }
 });
 
@@ -14,43 +22,45 @@ var BoardRow = React.createClass({
     this.props.onMouseEnter(x,y);
   },
   render: function(){
-    return(
-    <tr>
-      <Cell  x={0} y = {this.props.y} onMouseEnter={this.handleHover}/>
-      <Cell  x={1} y = {this.props.y} onMouseEnter={this.handleHover}/>
-      <Cell  x={2} y = {this.props.y} onMouseEnter={this.handleHover}/>
-      <Cell  x={3} y = {this.props.y} onMouseEnter={this.handleHover}/>
-      <Cell  x={4} y = {this.props.y} onMouseEnter={this.handleHover}/>
-      <Cell  x={5} y = {this.props.y} onMouseEnter={this.handleHover}/>
-      <Cell  x={6} y = {this.props.y} onMouseEnter={this.handleHover}/>
-      <Cell  x={7} y = {this.props.y} onMouseEnter={this.handleHover}/>
-      <Cell  x={8} y = {this.props.y} onMouseEnter={this.handleHover}/>
-      <Cell  x={9} y = {this.props.y} onMouseEnter={this.handleHover}/>
-    </tr>
+    var columns = this.props.columns
+    return (
+      <td>
+      { columns.map(function(object, i, index){
+        return <Cell value={object} key={i} x={this.props.x} y={arguments[1]} onMouseEnter={this.handleHover}/>
+      }, this)}
+    </td>
     )
   }
 })
 
 var Board = React.createClass({
+  getInitialState: function(){
+    return { data: []};
+  },
+  componentWillMount: function(){
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   test: function(x,y){
     console.log(x + ',' + y)
   },
   render: function(){
     var boardX = this.props.boardX;
     var boardY = this.props.boardY;
-
     return (
       <table className="gameBoard">
-        <BoardRow key={'tarp'}   y={0} onMouseEnter={this.test}/>
-        <BoardRow key={'tarp2'}  y={1} onMouseEnter={this.test}/>
-        <BoardRow key={'tarp3'}  y={2} onMouseEnter={this.test}/>
-        <BoardRow key={'tarp4'}  y={3} onMouseEnter={this.test}/>
-        <BoardRow key={'tarp5'}  y={4} onMouseEnter={this.test}/>
-        <BoardRow key={'tarp6'}  y={5} onMouseEnter={this.test}/>
-        <BoardRow key={'tarp7'}  y={6} onMouseEnter={this.test}/>
-        <BoardRow key={'tarp8'}  y={7} onMouseEnter={this.test}/>
-        <BoardRow key={'tarp9'}  y={8} onMouseEnter={this.test}/>
-        <BoardRow key={'tarp9'}  y={9} onMouseEnter={this.test}/>
+        { this.state.data.map(function(object, i){
+          return <BoardRow key={i} x={arguments[1]} columns={object} onMouseEnter={this.test}/>
+        }, this)}
       </table>
     )
   }
@@ -58,5 +68,6 @@ var Board = React.createClass({
 
 React.render(<Board
               boardX={[0,1,2,3,4,5,6,7,8,9]}
-              boardY={[0,1,2,3,4,5,6,7,8,9]}/>, document.getElementById('hello'));
+              boardY={[0,1,2,3,4,5,6,7,8,9]}
+              url= {'http://localhost:9292/board'} />, document.getElementById('game'));
 
